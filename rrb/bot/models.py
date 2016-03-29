551e84ade2c35db1f72322ad1d74f8b764a5d7ea
Christoph Matthies <christoph.matthies@gmail.com>
2016-03-29 16:47:12 +0200

    Changes to GithubPullRequest.

--- rrb/bot/models.py
+++ rrb/bot/models.py
@@ -4,14 +4,16 @@ from jsonfield import JSONField
 # Create your models here.
 class GitHubPullRequest(models.Model):
 	created_at = models.DateTimeField(auto_now_add=True)
-	reviewer_count = models.IntegerField()
-	# blank=True, null=True
-	pull_request_json = JSONField(default={})
-	diff = models.TextField(blank=True)
+	reviewer_count = models.IntegerField(blank=True, null=True)
+	pull_request_json = JSONField(blank=True, null=True)
 
 	class Meta:
 		verbose_name = 'Pull Request'
 		verbose_name_plural = 'Pull Requests'
 
 	def __str__(self):
-		return str(self.created_at) + str(self.reviewer_count)
+		if self.pull_request_json:
+			name = self.pull_request_json['title']
+		else:
+			name = 'EMPTY'
+		return name
